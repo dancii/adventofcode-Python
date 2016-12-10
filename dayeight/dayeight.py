94252766886c1222c0a4c451ddf244449ac6c6c1
dancii <gromati163@gmail.com>
2016-12-10 13:57:54 +0100

    dayeight in progress

--- dayeight/dayeight.py
+++ dayeight/dayeight.py
@@ -4,8 +4,9 @@ from collections import deque
 
 def main():
 
-    screen = deque([['.'] * 50, ['.'] * 6])
-    print(screen)
+    w, h = 50, 6
+    screen = [[0 for x in range(w)] for y in range(h)]
+    #print(screen)
 
     operations = open('input.txt').read().strip().split('\n')
 
@@ -13,14 +14,21 @@ def main():
         operation_list = operation.split(' ')
         if operation_list[0] == "rect":
             xy = ''.join(operation_list[1]).split('x')
-            x = int(xy[0])-1
-            y = int(xy[1])-1
+            y = int(xy[0])-1
+            x = int(xy[1])-1
             screen[x][y] = '#'
         elif operation_list[0] == 'rotate':
             if operation_list[1] == 'row':
-                pass
+                rotate_count = int(operation_list[2].split('=')[1])
+                for alist in screen:
+
             elif operation_list[1] == 'column':
-                pass
-    print(screen)
+                rotate_count = operation_list[2].split('=')[1]
+
+    for shit in screen:
+        print(shit)
+
+def rotate(l, n):
+    return l[n:] + l[:n]
 
 main()
